ec53c0139318d3c2010b1a6ed0852df288bb33bf
SabrinaFD97 <sabrinafernandezd97@gmail.com>
2026-09-24 20:25:29 -0300

    added high scores

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-const question = document.getElementById('question');
+const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -45,16 +46,22 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions)
     getNewQuestion();
 };
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        //go to end page
         return window.location.assign('/end.html');
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+    //Update the progress bar
+
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
@@ -83,6 +90,7 @@ choices.forEach(choice => {
         if(classToApply === "correct") {
             incrementScore(CORRECT_BONUS);
         }
+
 
         selectedChoice.parentElement.classList.add(classToApply);
 
